e44fdae4d9cdca3459404ec7b779087c9c5ba38c
David N <nguyen.david94579@gmail.com>
2021-03-19 21:49:51 -0700

    Turned appointments into appointment cards for prettier view.

---
+++ src/Components/AppointmentCardGroup.js
@@ -0,0 +1,57 @@
+import React, {useContext} from 'react'
+import {Card, CardContent, Dimmer, Loader} from 'semantic-ui-react'
+import {useQuery} from "@apollo/client";
+import gql from "graphql-tag";
+import {AuthContext} from "../context/auth";
+
+function AppointmentCardGroup() {
+
+    const {user} = useContext(AuthContext)
+
+    const {loading_bookings, data: {getUserBookingsHistory: bookings} = {}} =
+        useQuery(FETCH_USER_APP_BOOKINGS, {variables: {username: user ? user.username : null}})
+
+    return (
+        <Card.Group>
+            {
+                loading_bookings ? (
+                    <Dimmer active>
+                        <Loader/>
+                    </Dimmer>) : (
+                    bookings &&
+                    bookings.map((booking) => (
+                            <Card>
+                                <CardContent>
+                                    <Card.Header>
+                                        {booking.createdAt}
+                                    </Card.Header>
+                                    <Card.Meta>
+                                        {booking.confirmed.toString()}
+                                    </Card.Meta>
+                                    <Card.Description>
+                                        {booking.serviceType}
+                                    </Card.Description>
+                                </CardContent>
+                            </Card>
+                    ))
+                )
+            }
+        </Card.Group>
+
+    )
+}
+
+const FETCH_USER_APP_BOOKINGS =
+gql`
+    query getUserBookingsHistory($username: String!)
+    {
+        getUserBookingsHistory(username: $username)
+        {
+            createdAt
+            confirmed
+            serviceType
+        }
+    }
+`
+
+export default AppointmentCardGroup
--- src/Components/AuthAppointment.js
+++ src/Components/AuthAppointment.js
@@ -1,63 +1,20 @@
-import React, {useContext} from 'react'
-import gql from 'graphql-tag'
-import {useQuery} from "@apollo/client";
-import {AuthContext} from "../context/auth";
-import {Dimmer,  Item, Loader} from "semantic-ui-react";
+import React from 'react'
 import MakeAppointmentModal from "./MakeAppointmentModal";
+import AppointmentCardGroup from "./AppointmentCardGroup";
 
 
 function AuthAppointment({props}) {
 
-    const {user} = useContext(AuthContext)
-
-
-    const {loading_bookings, data: {getUserBookingsHistory: bookings} = {}} =
-        useQuery(FETCH_USER_APP_BOOKINGS, {variables: {username: user ? user.username : null}})
 
     return (
         <div>
             <div className='form-container'>
                 <MakeAppointmentModal props={props}/>
                 <h1> Your past bookings listed here </h1>
-                <ul>
-                    {
-                        loading_bookings ? (
-                            <Dimmer active>
-                                <Loader/>
-                            </Dimmer>) : (
-                            bookings &&
-                            bookings.map((booking, index) => (
-                                <li key={index}>
-                                    <Item>
-                                        <Item.Content>
-                                            <Item.Header as='h3'>{booking.createdAt}</Item.Header>
-                                            <Item.Header as='h3'>{booking.serviceType}</Item.Header>
-                                            <Item.Header as='h3'>{booking.confirmed}</Item.Header>
-                                        </Item.Content>
-                                    </Item>
-                                </li>
-                            ))
-                        )
-                    }
-                </ul>
+                <AppointmentCardGroup />
             </div>
         </div>
     )
 }
-
-
-const FETCH_USER_APP_BOOKINGS =
-gql`
-    query getUserBookingsHistory($username: String!)
-    {
-        getUserBookingsHistory(username: $username)
-        {
-            createdAt
-            confirmed
-            serviceType
-        }
-    }
-`
-
 
 export default AuthAppointment
