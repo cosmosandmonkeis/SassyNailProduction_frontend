08fd479defeb82a30427307ed977031fe7c1c5e9
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-05-26 20:17:24 -0700

    Removed need for getAUser Query

--- src/util/AdminRoute.js
+++ src/util/AdminRoute.js
@@ -1,55 +1,19 @@
 import React, {useContext} from 'react'
 import {Redirect, Route} from 'react-router-dom'
 import {AuthContext} from "../context/auth";
-import gql from "graphql-tag";
-import {useQuery} from "@apollo/client";
-import {Dimmer, Loader} from "semantic-ui-react";
 
 function AdminRoute({component: Component, ...rest}) {
     const {user} = useContext(AuthContext)
 
-    const {loading, data: {getAUser: userData} = {}} =
-        useQuery(FIND_USER_INFO, {
-            variables: user ? {username: user.username} : {username: ''}
-        })
-
     return (
         <div>
-            {
-                loading ? (
-                        <Dimmer active>
-                            <Loader/>
-                        </Dimmer>
-                    ) :
-                    <Route
-                        {...rest}
-                        render={props =>
-                            (userData && userData.admin === true) ? <Component {...props} /> : <Redirect to='/'/>}
-                    />
-            }
+            <Route
+                {...rest}
+                render={props =>
+                    (user && user.admin === true) ? <Component {...props} /> : <Redirect to='/'/>}
+            />
         </div>
     )
 }
-
-const FIND_USER_INFO = gql`
-    query getAUser(
-        $username: String!
-    ) {
-        getAUser(username: $username)
-        {
-            id
-            username
-            email
-            phonenumber
-            bookingsHistory {
-                status
-                createdAt
-                serviceType
-            }
-            createdAt
-            admin
-        }
-    }
-`
 
 export default AdminRoute
